aeadd9293dbde66ee01bda5fd6c2eae707e3103c
bhavesh <bhaveshpraveen10@gmail.com>
2021-01-24 13:10:24 +0530

    Add two urls. One for searching and other one for listing data.

---
+++ src/feed/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from feed.views import VideoListView, VideoSearchView
+
+urlpatterns = [
+    path('list/', VideoListView.as_view()),
+    path('search/', VideoSearchView.as_view()),
+]
